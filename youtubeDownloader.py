84702aaf7ac6e8ce1f16a76c3a971c56d69675bd
Min Thaw Zin <minthaw.zin11@gmail.com>
2020-02-21 15:33:13 +0630

    Added progress bar

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -2,6 +2,13 @@
 
 import pytube
 import os
+from progress.bar import Bar
+from time import sleep
+import progressbar
+import time
+from tqdm import tqdm
+
+import sys
 
 class youtubeDownloader():
 
@@ -12,20 +19,39 @@ class youtubeDownloader():
         else:
             return "Youtube Link needed"
 
+    def get_terminal_size():
+        """Return the terminal size in rows and columns."""
+        rows, columns = os.popen('stty size', 'r').read().split()
+        return int(rows), int(columns)
+
+    def display_progress_bar(bytes_received, filesize, ch='#', scale=0.55):
+        _, columns = youtubeDownloader.get_terminal_size()
+        max_width = int(columns * scale)
+
+        filled = int(round(max_width * bytes_received / float(filesize)))
+        remaining = max_width - filled
+        bar = ch * filled + ' ' * remaining
+        percent = round(100.0 * bytes_received / float(filesize), 1)
+        text = "Downloading:{bar}:{percent}%\r".format(bar=bar, percent=percent)
+        sys.stdout.write(text)
+        sys.stdout.flush()
+
     def on_progress(stream, chunk, file_handle, bytes_remaining):
-        total_size = stream.filesize
-        bytes_downloaded = total_size - bytes_remaining
-        percentage_of_completion = bytes_downloaded / total_size * 100
-        print(percentage_of_completion)
+        filesize = stream.filesize
+        bytes_received = filesize - bytes_remaining
+        youtubeDownloader.display_progress_bar(bytes_received, filesize)
 
     def getHighRes(link):
-        ytube_video = pytube.YouTube(link)
+        ytube_video = pytube.YouTube(link, on_progress_callback=youtubeDownloader.on_progress)
         highResVideo = ytube_video.streams.filter(progressive=True).order_by('resolution').desc().first()
+        print("Fetching streams...")
         return highResVideo
 
     def getAudioOnly(link):
-        video = pytube.YouTube(link)
+
+        video = pytube.YouTube(link, on_progress_callback=youtubeDownloader.on_progress)
         audioOnly = video.streams.filter(only_audio=True).all()
+        print("Fetching streams...")
         return audioOnly[0]
 
 if __name__ == '__main__':
